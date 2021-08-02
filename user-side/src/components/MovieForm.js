499f10e06568f4f0913f0a674f2c014a4eb68f50
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-02 19:28:23 +0530

    Adding movie form

---
+++ user-side/src/components/MovieForm.js
@@ -0,0 +1,31 @@
+import './MovieForm.css';
+
+export default function MovieForm() {
+
+    const handleSubmit = () => {
+        alert("sent");
+    }
+    return (
+        <div>
+            <form>
+                <ul className='container'>
+                    <li>
+                        <input type='text' placeholder='Movie Title'></input><br />
+                    </li>
+                    <li>
+                        <textarea rows='15' cols='20' placeholder='Summary' className='form-control'></textarea><br />
+                    </li>
+                    <li>
+                        <input type='text' placeholder='Cast'></input><br />
+                    </li>
+                    <li>
+                        <input type='text' placeholder='Genre'></input><br />
+                    </li>
+                    <li>
+                        <button type='submit' onClick={handleSubmit}>Submit</button>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    );
+}

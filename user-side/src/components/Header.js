91b3e436f662bf8c972e1d3779f4cf561522957f
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-03 12:31:37 +0530

    Adding conditional rendering through props value

--- user-side/src/components/Header.js
+++ user-side/src/components/Header.js
@@ -1,23 +1,29 @@
 import './Header.css';
 import logo from './images/placeholder.png';
-export default function Header() {
+export default function Header(props) {
     return (
         <div className="background">
-                
-                <div className="container1">
+ 
+            <div className="container1">
                 <img src={logo} alt="Logo" />
-                <div className='container2'>
-                <input type="text" id="form1" placeholder="Search..." /><select>
-                    <option>Genre</option>
-                    <option>All</option>
-                </select>
-                <button type="submit" className="btn"><i className="fa fa-search"></i></button>
-                
-                
-                </div>
-                <button className="add-movie">Add a Movie</button>
-                </div>
-
+               
+                    {props.searchBar === "yes" ? (
+                        <div className='container2'>
+                            <input type="text" id="form1" placeholder="Search..." />
+                            <select>
+                                <option>Genre</option>
+                                <option>All</option>
+                            </select>
+                            <button type="submit" className="btn"><i className="fa fa-search"></i></button>
+                        </div>
+                    ) : null}
+                    
+                    {props.addButton === "yes" ? (
+                        <button className="add-movie">Add a Movie</button>
+                    ) : null}
+ 
+            </div>
+ 
         </div>
     )
 }

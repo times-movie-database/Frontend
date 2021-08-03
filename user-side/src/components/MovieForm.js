bb0658ad0e57dcd355badbb06ca285fb2d888919
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-03 16:42:16 +0530

    Adding onchange event listeners

--- user-side/src/components/MovieForm.js
+++ user-side/src/components/MovieForm.js
@@ -1,7 +1,32 @@
 import './MovieForm.css';
-
+import { useState } from 'react';
 export default function MovieForm() {
-
+    const initialState = {
+        title: "",
+        summary: "",
+        cast: [],
+        genres: []
+    }
+    const [movie, setMovie] = useState(initialState)
+    const validateTitle=(title,value)=>{
+        if(value.length<50)
+            setMovie({...movie,[title]:value});
+        else
+         return;
+    }
+    const handleFeildChange = (event) => {
+        switch (event.target.name) {
+            case 'title': validateTitle(event.target.name,event.target.value);
+                         console.log(event.target.value)
+                          break;
+            case 'summary': console.log(event.target.value)
+                            break;
+            case 'cast': console.log(event.target.value)
+                         break;
+            case 'genres': console.log(event.target.value)
+                           break;
+        }
+    }
     const handleSubmit = () => {
         alert("sent");
     }
@@ -10,16 +35,16 @@ export default function MovieForm() {
             <form>
                 <ul className='container'>
                     <li>
-                        <input type='text' placeholder='Movie Title'></input><br />
+                        <input name='title' type='text' placeholder='Movie Title' value={movie.title} onChange={handleFeildChange} isRequired></input><br />
                     </li>
                     <li>
-                        <textarea rows='15' cols='20' placeholder='Summary' className='form-control'></textarea><br />
+                        <textarea name='summary' rows='15' cols='20' placeholder='Summary' value={movie.summary} className='form-control' onChange={handleFeildChange}></textarea ><br />
                     </li>
                     <li>
-                        <input type='text' placeholder='Cast'></input><br />
+                        <input name='cast' type='text' placeholder='Cast' value={movie.cast}onChange={handleFeildChange}></input><br />
                     </li>
                     <li>
-                        <input type='text' placeholder='Genre'></input><br />
+                        <input name='genres' type='text' placeholder='Genre' value={movie.genres} onChange={handleFeildChange}></input><br />
                     </li>
                     <li>
                         <button type='submit' onClick={handleSubmit}>Submit</button>

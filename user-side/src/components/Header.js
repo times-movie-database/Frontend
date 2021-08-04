48bc5922229970902261206e8bd4ee60ed41782b
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-04 19:13:37 +0530

    Linking add movie button to movie form

--- user-side/src/components/Header.js
+++ user-side/src/components/Header.js
@@ -1,29 +1,32 @@
 import './Header.css';
 import logo from './images/placeholder.png';
 export default function Header(props) {
+    const redirectToAddMovie = () => {
+        window.location.href = '/movie/add';
+    }
     return (
         <div className="header background">
- 
+
             <div className="container1">
                 <img src={logo} alt="Logo" />
-               
-                    {props.searchBar === "yes" ? (
-                        <div className='container2'>
-                            <input type="text" id="form1" placeholder="Search..." />
-                            <select>
-                                <option>Genre</option>
-                                <option>All</option>
-                            </select>
-                            <button type="submit" className="btn"><i className="fa fa-search"></i></button>
-                        </div>
-                    ) : null}
-                    
-                    {props.addButton === "yes" ? (
-                        <button className="add-movie">Add a Movie</button>
-                    ) : null}
- 
+
+                {props.searchBar === "yes" ? (
+                    <div className='container2'>
+                        <input type="text" id="form1" placeholder="Search..." />
+                        <select>
+                            <option>Genre</option>
+                            <option>All</option>
+                        </select>
+                        <button type="submit" className="btn"><i className="fa fa-search"></i></button>
+                    </div>
+                ) : null}
+
+                {props.addButton === "yes" ? (
+                    <button className="add-movie" onClick={redirectToAddMovie}>Add a Movie</button>
+                ) : null}
+
             </div>
- 
+
         </div>
     )
 }

c0bc98f9dc05d9833ea918458afa2180bbfb53fe
yashdani55 <yashdani55@gmail.com>
2021-08-05 15:54:21 +0530

    fixed the alignment issue in cards

--- user-side/src/components/Header.js
+++ user-side/src/components/Header.js
@@ -1,32 +1,33 @@
-import './Header.css';
-import logo from './images/placeholder.png';
+import "./Header.css";
+import logo from "./images/placeholder.png";
 export default function Header(props) {
-    const redirectToAddMovie = () => {
-        window.location.href = '/movie/add';
-    }
-    return (
-        <div className="header background">
+  const redirectToAddMovie = () => {
+    window.location.href = "/movie/add";
+  };
+  return (
+    <div className="header background">
+      <div className="container1">
+        <img src={logo} alt="Logo" />
 
-            <div className="container1">
-                <img src={logo} alt="Logo" />
+        {props.searchBar === "yes" ? (
+          <div className="container2">
+            <input type="text" id="form1" placeholder="Search..." />
+            <select className="slt">
+              <option className="opt">Genre</option>
+              <option className="opt">All</option>
+            </select>
+            <button type="submit" className="btn">
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
+        ) : null}
 
-                {props.searchBar === "yes" ? (
-                    <div className='container2'>
-                        <input type="text" id="form1" placeholder="Search..." />
-                        <select>
-                            <option>Genre</option>
-                            <option>All</option>
-                        </select>
-                        <button type="submit" className="btn"><i className="fa fa-search"></i></button>
-                    </div>
-                ) : null}
-
-                {props.addButton === "yes" ? (
-                    <button className="add-movie" onClick={redirectToAddMovie}>Add a Movie</button>
-                ) : null}
-
-            </div>
-
-        </div>
-    )
+        {props.addButton === "yes" ? (
+          <button className="add-movie" onClick={redirectToAddMovie}>
+            Add a Movie
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
 }

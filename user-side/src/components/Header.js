ec2787c2ecae4857423ed01eae08fd7dcefa5c14
yashdani55 <yashdani55@gmail.com>
2021-08-09 21:45:21 +0530

    added event listeners to the header component

--- user-side/src/components/Header.js
+++ user-side/src/components/Header.js
@@ -1,22 +1,32 @@
 import "./Header.css";
 import logo from "./images/placeholder.png";
+import { useState } from "react";
 export default function Header(props) {
   const redirectToAddMovie = () => {
     window.location.href = "/movie/add";
   };
+  const [keyword,setKeyword]=useState('');
+  const searchHandler=(event)=>{
+        setKeyword(event.target.value);
+  };
+  const submitHandler=(event)=>{
+        const movietitle=keyword;
+        console.log(movietitle);    
+  };
   return (
+    
     <div className="header background">
       <div className="container1">
         <img src={logo} alt="Logo" />
 
         {props.searchBar === "yes" ? (
           <div className="container2">
-            <input type="text" id="form1" placeholder="Search..." />
+            <input type="text" id="form1" placeholder="Search..." onChange={searchHandler} />
             <select className="slt">
               <option className="opt">Genre</option>
               <option className="opt">All</option>
             </select>
-            <button type="submit" className="btn">
+            <button type="submit" className="btn" onClick={submitHandler}>
               <i className="fa fa-search"></i>
             </button>
           </div>

c660735410f7d4cc77cc2912d177cc68ef3d38c1
yashdani55 <yashdani55@gmail.com>
2021-08-11 13:16:31 +0530

    added dynamic cards using json db

--- user-side/src/components/SearchScreen.js
+++ user-side/src/components/SearchScreen.js
@@ -1,8 +1,25 @@
+import Card from './Card';
+import './SearchScreen.css';
+import { useState,useEffect } from 'react';
+import { getmovie } from '../Services';
 export default function SearchScreen(props){
+    const [movies,setMovies]=useState([]);
+    useEffect(() => {
+        getmovie(response => setMovies(response.data));
+        
+        },[])
     return(
-    <div>
-        Search Results
+    <div id='container2'>
+        <div className='searchgrid'>
+            {movies.map((movie,index)=>
+             <Card className="card" title={movie.title} rating={movie.rating}></Card>
+            )
+            }
+            
+            </div>
 
     </div>)
 
 }
+
+

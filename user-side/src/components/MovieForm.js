d406d5b1a2fa43fbbfefb9c2c48fd271305beb44
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-11 11:46:36 +0530

    Updating movie form look and behaviour

--- user-side/src/components/MovieForm.js
+++ user-side/src/components/MovieForm.js
@@ -1,5 +1,6 @@
 import './MovieForm.css';
 import { useState } from 'react';
+import { useRef } from "react";
 import Select from 'react-select'
 import { sendMovieToDB } from '../Services';
 
@@ -8,12 +9,11 @@ export default function MovieForm() {
     const TITLE_MAX = 100;
     const SUMMARY_MAX = 500;
     const CAST_NAME_MAX = 50;
-
+    const dropdownSelectInputRef = useRef();
     const initialMovieState = {
         title: "",
         summary: "",
-        cast: [],
-        genres: []
+        genreIdList: []
     }
     const [movie, setMovie] = useState(initialMovieState);
 
@@ -60,11 +60,11 @@ export default function MovieForm() {
             setTitleError({ ...titleError, limitExceeds: true });
         }
         /*If the feild is empty*/
-        if(value.length===0){
-            setTitleError({...titleError,fieldEmpty : true});
+        if (value.length === 0) {
+            setTitleError({ ...titleError, fieldEmpty: true });
         }
-        else{
-            setTitleError({...titleError,fieldEmpty : false})
+        else {
+            setTitleError({ ...titleError, fieldEmpty: false })
         }
 
 
@@ -91,7 +91,7 @@ export default function MovieForm() {
         const regex = /^[,a-zA-Z ]/;
         return regex.test(character);
     }
-
+   
     const validateCastName = (csv) => {
         const lastChar = csv.charAt(csv.length - 1);
         const newCast = parseLastNameInCSV(csv);
@@ -117,6 +117,7 @@ export default function MovieForm() {
         if (lastChar === ',') {
             if (castName.length > 0) {
                 setCastNameCSV(csv);
+                setCastError({ ...castError, blankName: false });
             }
             else {
                 console.log("cast name shouldn't be blank");
@@ -137,46 +138,62 @@ export default function MovieForm() {
             default: console.log("Default Case");
         }
     }
-    
+
     const handleDropdownChange = (e) => {
-        setMovie({ ...movie, genres: e });
-        if(e.length>0){
-            setGenreError({...genreError, fieldEmpty : false});
+        /*e is array of genre object*/
+        const genreID = e.map((genre) => {
+            return genre.id;
+        })
+        setMovie({ ...movie, genreIdList: genreID });
+        if (e.length > 0) {
+            setGenreError({ ...genreError, fieldEmpty: false });
         }
-        else{
-            setGenreError({...genreError, fieldEmpty : true});
+        else {
+            setGenreError({ ...genreError, fieldEmpty: true });
         }
-    }
-   
-    const createCastList = () => {
-        setMovie(
-            () => {
-                return {
-                    cast: castNameCSV.split(',')
-                };
-            }
-        )
     }
 
+    const handleReset = () => {
+        /*Reset movie form state*/
+        setMovie(initialMovieState);
+        setCastNameCSV("");
+
+        /*Reset every feild*/
+        Array.from(document.querySelectorAll("input")).forEach(
+            input => (input.value = "") );
+        dropdownSelectInputRef.current.select.clearValue();
+
+        /*reset error properties for each feild*/
+        setTitleError( { ...titleError, fieldEmpty:true } );
+        setCastError( { ...castError, fieldEmpty:true } );
+        setSummaryError( { ...summaryError, fieldEmpty:true } );
+        setGenreError( { ...genreError, fieldEmpty:true } );
+        
+    }
     const handleSubmit = (e) => {
-        createCastList();
         e.preventDefault();
-        if ( titleError.fieldEmpty || genreError.fieldEmpty ) {
+        if (titleError.fieldEmpty || genreError.fieldEmpty) {
             setFeildRequired(true);
         }
         else {
+            const movieDetails = {
+                title: movie.title,
+                summary: movie.summary,
+                casteList: castNameCSV.split(','),
+                genreIdList: movie.genreIdList
+            };
             setFeildRequired(false);
             sendMovieToDB(
-                movie,
-                () => {alert("Success");
-                setMovie({...movie,initialMovieState}); },
+                movieDetails,
+                () => {
+                    alert("Success");
+                    handleReset();
+                },
                 (error) => {
                     alert(`Error ${error}`);
                 }
             );
         }
-        console.log(movie.cast);
-        console.log(movie);
 
     }
 
@@ -191,13 +208,13 @@ export default function MovieForm() {
                         <input name='title' id='title' type='text' placeholder='Movie Title' value={movie.title} onChange={handleFeildChange}></input><br />
                     </div>
                     {titleError.limitExceeds ? <div className='warning'>Movie title should not be more than 100 characters</div> : null}
-                    { ( titleError.fieldEmpty && feildRequired )?<div className='warning shake-text'>Movie title cannot be blank</div> : null}
+                    {(titleError.fieldEmpty && feildRequired) ? <div className='warning shake-text'>Movie title cannot be blank</div> : null}
 
 
                     <div className='form-group'>
                         <textarea name='summary' id='summary' rows='15' cols='20' placeholder='Summary' value={movie.summary} className='form-control' onChange={handleFeildChange}></textarea ><br />
                     </div>
-                    { summaryError.limitExceeds ? <div className='warning'>Try to keep summary crisp. Can't be more than 500 characters long. </div> : null}
+                    {summaryError.limitExceeds ? <div className='warning'>Try to keep summary crisp. Can't be more than 500 characters long. </div> : null}
 
                     <div className='form-group'>
                         <input name='cast' id='cast' type='text' placeholder='Cast' value={castNameCSV} onChange={handleFeildChange}></input><br />
@@ -208,18 +225,19 @@ export default function MovieForm() {
                     <div className='form-group'>
                         <span className="required">*</span>
                         <Select name='genres' id='genres' isMulti
+                            ref = { dropdownSelectInputRef }
                             placeholder='Choose Genres'
                             getOptionLabel={option => option.name}
                             getOptionValue={option => option.id}
                             options={genreList}
                             onChange={handleDropdownChange} /><br />
                     </div>
-                    { ( genreError.fieldEmpty && feildRequired )?<div className='warning shake-text'>Please select atleast one genre</div> : null}
+                    {(genreError.fieldEmpty && feildRequired) ? <div className='warning shake-text'>Please select atleast one genre</div> : null}
                     <div className='btn-center'>
                         <button type='submit' onClick={handleSubmit} className='btn'>Submit</button>
                     </div>
-                
-                    <p className={(feildRequired)?" bottom-text shake-text":"bottom-text"}>
+
+                    <p className={(feildRequired) ? " bottom-text shake-text" : "bottom-text"}>
                         <strong>Note: </strong> <span className="required">*</span> marked feilds are required
                     </p>
                 </form>

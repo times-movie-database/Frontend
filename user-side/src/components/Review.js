06a4ccee8c9e78a43142938c2777e1ee924ae96e
yashdani55 <yashdani55@gmail.com>
2021-08-16 03:16:09 +0530

    created movie details component

--- user-side/src/components/Review.js
+++ user-side/src/components/Review.js
@@ -1,10 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import "./Review.css";
+import { useState } from "react";
 export default function Review(props) {
-    return (
-        <div>
-            <div>{props.timestamp}</div>
-            <div>{props.children}</div>
-        </div>
-    )
+  const text = props.children;
+  const [Readmore, setReadmore] = useState(true);
+  const toggleReadmore = () => {
+    setReadmore(!Readmore);
+  };
+  return (
+    <div className="review">
+      <div className="timestamp">
+        19-02-1998
+        <br/>
+        <br/>
+      </div>
+      <div className="content">
+        {Readmore ? text.slice(0,150) : text}
+        <span onClick={toggleReadmore} className="read-more">
+          {Readmore ? "...read more" : " Show Less"}
+        </span>
+      </div>
+    </div>
+  );
 }

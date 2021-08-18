2948033472291dec7a6dedaf0481592ec19f17a2
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-18 11:39:32 +0530

    Adding put method for edit movie details

--- user-side/src/Services.js
+++ user-side/src/Services.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
-export function sendMovieToDB(movie, successCB, errorCB){
-    axios.post(process.env.REACT_APP_POST_MOVIE_ENDPOINT,movie).then(successCB).catch(errorCB);
+export function sendMovieToDB(movieDetails, successCB, errorCB){
+    axios.post(process.env.REACT_APP_POST_MOVIE_ENDPOINT,movieDetails).then(successCB).catch(errorCB);
 }
 
 export async function getAllGenre( successCB, errorCB ){
     const response = axios.get(process.env.REACT_APP_GET_GENRE_ENDPOINT);
     return response;
 }
+export function updateMovieInDB( movieID, movieDetails , successCB, errorCB){
+    axios.put(process.env.REACT_APP_PUT_MOVIE_ENDPOINT+movieID, movieDetails).then(successCB).catch(errorCB);
+} 
 export function getmovie(successCB,l=1000){
     const URL="http://localhost:4200/movies"
     axios.get(URL,{params:{_limit:l}}).then(successCB)

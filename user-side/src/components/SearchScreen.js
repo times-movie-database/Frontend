ac434df8fc8cc0b39c7cd61b5328d8cf5ea7f05c
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-20 14:52:28 +0530

    Adding conditional rendering for empty results

--- user-side/src/components/SearchScreen.js
+++ user-side/src/components/SearchScreen.js
@@ -1,67 +1,78 @@
 import Card from "./Card";
 import "./SearchScreen.css";
 import { useState, useEffect } from "react";
-import { searchMovie} from "../Services";
-import { getAllGenre} from '../Services';
+import { searchMovie } from "../Services";
+import { getAllGenre } from '../Services';
 import ErrorBoundary from "./ErrorBoundary";
 import SearchBar from "./SearchBar";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Header from "./Header";
+import EmptySearchScreen from "./EmptySearchScreen";
 export default function SearchScreen(props) {
   const [movies, setMovies] = useState([]);
-  const [genreList,setGenreList]=useState([]);
-  const [selectedGenre,setSelectedGenre]=useState('All');
-  const [pageNumber,setPageNumber]=useState(0);
+  const [genreList, setGenreList] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState('All');
+  const [pageNumber, setPageNumber] = useState(0);
   const location = useLocation();
-  const {searchKeyword}=location.state;
-   /*get genre list from server*/
-   if (genreList.length===0){
+  const { searchKeyword } = useParams();
+
+  const isSearchKeywordEmpty = () => {
+    console.log(searchKeyword.length);
+    const regex = /[\s\s+/g]/;
+    return regex.test(searchKeyword) && searchKeyword.length;
+  }
+
+  /*get genre list from server*/
+  if (genreList.length === 0) {
     getAllGenre().then((res) => {
       setGenreList(res.data)
-   }
-    )}
-    const handleGenre=(event)=>{
-      setSelectedGenre(event.target.value);
-      setPageNumber(0);
+    }
+    )
+  }
+  const handleGenre = (event) => {
+    setSelectedGenre(event.target.value);
+    setPageNumber(0);
   }
   useEffect(() => {
-    searchMovie(searchKeyword,selectedGenre,pageNumber,(response) => setMovies(response.data));
-  }, [searchKeyword,selectedGenre,pageNumber]);
-  
+    searchMovie(searchKeyword, selectedGenre, pageNumber, (response) => setMovies(response.data));
+  }, [searchKeyword, selectedGenre, pageNumber]);
+
   return (
-    
+
     <div>
       <ErrorBoundary>
-              <Header searchBar="yes" addButton="yes" />
-            </ErrorBoundary>
-    <div className="search">
-      
-      <div className="show-result">Results for "{searchKeyword}"</div>
-      <label htmlFor="genre-search" className="text">Search Result in </label>
+        <Header searchBar="yes" addButton="yes" />
+      </ErrorBoundary>
+
+      {isSearchKeywordEmpty() ? <EmptySearchScreen/> :
+        <div className="search">
+
+          <div className="show-result">Results for "{searchKeyword}"</div>
+          <label htmlFor="genre-search" className="text">Search Result in </label>
           <select className="genre-menu" onChange={handleGenre}>
             <option value='All'>All</option>
-            {genreList.map((genre)=><option className="opt" key={genre.id} value={genre.name}>{genre.name}</option>)}
+            {genreList.map((genre) => <option className="opt" key={genre.id} value={genre.name}>{genre.name}</option>)}
           </select>
-      <div id="container">
-        {movies?
-          <div className="searchgrid">
-            <InfiniteScroll dataLength={20} next={()=>(pageNumber+1)}>
-          {movies.map((movie, index) => (
-            <ErrorBoundary>
-              <Card
-                className="card"
-                title={movie.title}
-                rating={movie.rating}
-                id={movie.id}
-                key={index}
-              ></Card>
-            </ErrorBoundary>
-          ))}
-          
-        </InfiniteScroll></div>:<div className="show-result">No Result Found</div>}
-      </div>
-    </div>
+          <div id="container">
+            {movies ?
+              <div className="searchgrid">
+                <InfiniteScroll dataLength={20} next={() => (pageNumber + 1)}>
+                  {movies.map((movie, index) => (
+                    <ErrorBoundary>
+                      <Card
+                        className="card"
+                        title={movie.title}
+                        rating={movie.rating}
+                        id={movie.id}
+                        key={index}
+                      ></Card>
+                    </ErrorBoundary>
+                  ))}
+
+                </InfiniteScroll></div> : <div className="show-result">No Result Found</div>}
+          </div>
+        </div>}
     </div>
   );
 }

8f704399cef291b12b69f8ff0ca13d5bd4f2e168
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-20 15:44:32 +0530

    Adding styles to Empty Search Screen Component

--- user-side/src/components/EmptySearchScreen.js
+++ user-side/src/components/EmptySearchScreen.js
@@ -1,8 +1,16 @@
 export default function EmptySearchScreen(){
+    const style = {
+        display:'flex',
+        flexDirection: 'column',
+        alignItems:'center',
+        paddingTop : '10%',
+        paddingLeft:'10%',
+        paddingRight:'10%'
+      };
     return(
-        <div class="article">
-            <h3>Oops! Looks like you left the search bar blank</h3>
-    <p>Search  by typing a movie title or some phrase in the search box at the top of this page.</p>
+        <div class="article" style={style}>
+            <h3>Oops! Looks like you left the search bar empty !!</h3>
+    <p>Search movies by typing the movie title in the search box above.</p>
     <p>Or, check out TMDb users’ <a href="/">Top 10 Rated Movies</a> .</p>
         </div>
     )

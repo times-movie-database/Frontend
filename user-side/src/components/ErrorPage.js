1b2499559f4d25ce785019fe68030f7c2d4fcdc2
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-26 11:12:14 +0530

    Adding an error page component

---
+++ user-side/src/components/ErrorPage.js
@@ -0,0 +1,16 @@
+export default function ErrorPage(){
+    const style = {
+        display:'flex',
+        flexDirection: 'column',
+        alignItems:'center',
+        paddingTop : '10%',
+        paddingLeft:'10%',
+        paddingRight:'10%'
+      };
+    return(
+        <div class="article" style={style}>
+            <h3>Oops! Some error creeped.</h3>
+    <p><a href="/">Go to home page</a> .</p>
+        </div>
+    )
+}

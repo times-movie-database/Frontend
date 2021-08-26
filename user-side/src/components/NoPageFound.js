d1b0b0801b41d1ad4e9ad7fe2800987b3c86b240
gitinintoit <ravi.shyam@timesinternet.in>
2021-08-26 12:50:21 +0530

    Adding home page redirection

--- user-side/src/components/NoPageFound.js
+++ user-side/src/components/NoPageFound.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const NoPageFound = () => {
-  return <h1 style={{textAlign:'center'}}>404 Page Not Found </h1>;
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>404 Page Not Found </h1>
+            <p style={{ textAlign: 'center' }}><a href="/">Go to home page</a> </p>
+        </div>);
 }
 
 export default NoPageFound;
